0f76dec0920a75af6a88537ec7b7faa7b806a49f
pmNiko <nikolas090189@gmail.com>
2022-08-16 18:54:06 -0300

    Se creo el modelo Task

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,20 +1,19 @@
-import { v4 } from "uuid";
-import { getConnection } from "../server/db.js";
+import * as Task from "../model/Task.js";
 
 export const getAll = async (__, res) => {
-  const tasks = await Tas;
-  res.json({ tasks, count: tasks?.length });
+  const { tasks, count } = await Task.all();
+  res.json({ tasks, count });
 };
 
 export const count = async (__, res) => {
-  const tasks = await getConnection().get("tasks").value();
-  res.json({ count: tasks?.length });
+  const count = await Task.count();
+  res.json({ count });
 };
 
 export const findByID = async (req, res) => {
   const id = req.params.id;
 
-  const task = await getConnection().get("tasks").find({ id }).value();
+  const task = await Task.findByID(id);
 
   if (!task) return res.status(404).json({ message: "Tarea no encontrada." });
 
@@ -24,44 +23,30 @@ export const findByID = async (req, res) => {
 export const create = async (req, res) => {
   const { title, description } = req.body;
 
-  const newTask = {
-    id: v4(),
-    title,
-    description,
-  };
+  const id = await Task.create(title, description);
 
-  const db = await getConnection();
-  await db.get("tasks").push(newTask).write();
-
-  res.status(201).json(newTask.id);
+  res.status(201).json(id);
 };
 
 export const update = async (req, res) => {
   const id = req.params.id;
   const { title, description } = req.body;
 
-  const db = await getConnection();
+  const idTask = await Task.update(id, title, description);
 
-  const task = await db.get("tasks").find({ id }).value();
-
-  if (!task)
+  if (!idTask)
     return res.status(404).json({ message: "La tarea no fue encontrada." });
 
-  await db.get("tasks").find({ id }).assign({ title, description }).write();
-
-  res.status(200).json({ id });
+  res.status(200).json({ id: idTask });
 };
 
 export const remove = async (req, res) => {
   const id = req.params.id;
-  const db = await getConnection();
 
-  const task = await db.get("tasks").find({ id }).value();
+  const idTask = await Task.remove(id);
 
-  if (!task)
+  if (!idTask)
     return res.status(404).json({ message: "La tarea no fue encontrada." });
 
-  await db.get("tasks").remove({ id }).write();
-
-  res.status(202).json(id);
+  res.status(202).json(idTask);
 };
--- src/model/Task.js
+++ src/model/Task.js
@@ -5,3 +5,48 @@ export const all = async () => {
   const tasks = await getConnection().get("tasks").value();
   return { tasks, count: tasks.length };
 };
+
+export const count = async () => {
+  const tasks = await getConnection().get("tasks").value();
+  return tasks.length;
+};
+
+export const findByID = async (id) => {
+  const task = await getConnection().get("tasks").find({ id }).value();
+  return task;
+};
+
+export const create = async (title, description) => {
+  const newTask = {
+    id: v4(),
+    title,
+    description,
+  };
+  const db = await getConnection();
+  await db.get("tasks").push(newTask).write();
+
+  return newTask.id;
+};
+
+export const update = async (id, title, description) => {
+  const db = await getConnection();
+  const task = await db.get("tasks").find({ id }).value();
+
+  if (!task) return;
+
+  await db.get("tasks").find({ id }).assign({ title, description }).write();
+
+  return id;
+};
+
+export const remove = async (id) => {
+  const db = await getConnection();
+
+  const task = await db.get("tasks").find({ id }).value();
+
+  if (!task) return;
+
+  await db.get("tasks").remove({ id }).write();
+
+  return id;
+};
